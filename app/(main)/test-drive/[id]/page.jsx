fc2d8c8560ab09dee3fda17e5f5fed1c52405b45
Raunik47 <rauniksingh787878@gmail.com>
2025-05-29 01:15:59 +0530

    car booking has been complelte

--- app/(main)/test-drive/[id]/page.jsx
+++ app/(main)/test-drive/[id]/page.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import { getCarById } from "@/actions/car-listing";
+import { notFound } from "next/navigation";
+import { TestDriveForm } from "./_components/test-drive-form";
+
+
 
 export async function generateMetadata() {
   return {
-    title: `Book Test Drive | Vehial`,
+    title: `Book Test Drive | Vehiql`,
     description: `Schedule a test drive in few seconds`,
   };
 }
 
-const TestDrivePage = ({ params }) => {
-  return <div>TestDrivePage</div>;
-};
+export default async function TestDrivePage({ params }) {
+  // Fetch car details
+  const { id } = params;
+  const result = await getCarById(id);
 
-export default TestDrivePage;
+  // If car not found, show 404
+  if (!result.success) {
+    notFound();
+  }
+
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-6 gradient-title">Book a Test Drive</h1>
+      <TestDriveForm
+        car={result.data}
+        testDriveInfo={result.data.testDriveInfo}
+      />
+    </div>
+  );
+}
